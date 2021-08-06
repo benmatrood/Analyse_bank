bbd1a954fc1cd2bee3a9c6e62402c2137643e1ea
benmatrood <31375808+benmatrood@users.noreply.github.com>
2021-08-06 10:53:49 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -3,54 +3,44 @@ import  pandas as pd
 import  numpy as np
 from pandas import DataFrame
 from flask import Flask,render_template, request, url_for, jsonify , redirect
-from flaskext.mysql import MySQL 
-import pymysql
 import re
 import random
+import sqlite3
 
 app = Flask(__name__)
 
-mysql = MySQL()
-   
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'prediction'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+# mysql = MySQL()
+
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = ''
+# app.config['MYSQL_DATABASE_DB'] = 'prediction'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# mysql.init_app(app)
+
 
 @app.route("/")
-def hello_world():
+def main():
     return render_template('index.html')
+
+@app.route("/prediction")
+def hello_word():
+    return render_template('prediction.html')
 
 @app.route("/output",methods=['GET','POST'])
 def output():
-
-    #ouverture du model en lecture
-    f = open("model.sav", "rb")
-
-    #chargement du model en lecture
+    f = open("model.sav", "rb")   
     mdl = pickle.load(f)
-
-    #fermeture du fichier du fichier
     f.close()
-
     #fonction de prediction du
     def y_pred(X):
         y_pred = mdl.predict(X)
         return(y_pred)
-
-
     #fonction qui retourne le resultat final de la prédiction
     def resultat_finale(data):
-        d = np.transpose(DataFrame(data)) 
-        a = y_pred(d)
-        a = str(a).strip('[]')
-        print(int(a))
+        a = np.transpose(DataFrame(data)) 
+        a = str(y_pred(a)).strip('[]')
         return(int(a))
-    
-    data = request.json.values()
-
-    a= resultat_finale(data)
+    a= resultat_finale(request.json.values())
     if a==1:
         phrase ='<h5>Ce client est suceptible de quitter le service de la carte bancaire</h5>'
     elif  a==0:
@@ -61,70 +51,33 @@ def output():
 
 @app.route("/echantillon",methods=['GET', 'POST'])
 def echantillon():
-    if request.method == 'POST': 
-        def toto():
-            nb_ehantillon = int(request.form['input_number'])
-            return nb_ehantillon
-    return render_template('echantillon.html',toto =toto())
 
+    return render_template('echantillon.html')
 
-#retourne le datafame selectionner depuis la db au bon format
-def fdata_final():
-    conn = mysql.connect()
-    cursor =conn.cursor()
-    nb = 20
-    cursor.execute("SELECT  Months_on_book,Months_Inactive_12_mon,Credit_Limit,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1, Attrition_Flag FROM  client order by rand() LIMIT {} ".format(nb))   
-    data = cursor.fetchall()
-    cursor.close()
-
-   
-    # print (type(data))
-    # data=np.array(data)
-    # data=pd.DataFrame(data, columns=['Months_Inactive_12_mon','Months_Inactive_12_mon','Credit_Limit','Avg_Open_To_Buy','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt','Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Attrition_Flag'])
-    # data = DataFrame(data)
-    return(data)
-
-
-
-def retoune_prediction():
-    #ouverture du model en lecture
-    f = open("model.sav", "rb")
-    #chargement du model en lecture
-    mdl = pickle.load(f)
-    #fermeture du fichier du fichier
-    f.close()
-    #fonction de prediction 
-    data=np.array(fdata_final())
-    data=pd.DataFrame(data, columns=['Months_Inactive_12_mon','Months_Inactive_12_mon','Credit_Limit','Avg_Open_To_Buy','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt','Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Attrition_Flag'])
-    data = DataFrame(data)
-    
-    d = data.drop('Attrition_Flag',axis=1)
-    y_pred = mdl.predict(d)
-    y_pred = np.array(y_pred)
-    y_pred = pd.DataFrame(y_pred, columns=['Prédiction'])
-    y_pred =DataFrame(y_pred)
-    print(type(y_pred))
-    return y_pred
-
-
-
-@app.route("/home",methods=['GET', 'POST'])
+@app.route("/resultat",methods=['POST'])
 def output_1():
-    # fdata_final() 
-    # rst = pd.concat([fdata_final(),retoune_prediction()], axis=1)
-    headings = ('Months_Inactive_12_mon','Months_Inactive_12_mon','Credit_Limit','Avg_Open_To_Buy','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt','Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Attrition_Flag')
-    retoune_prediction()
-    return render_template('home.html',headings= headings ,data=fdata_final(),y_pred=retoune_prediction())    
-
-
-
-
-
-   
-
-
-
-
+    nb = request.form['input_number']
+    conn = sqlite3.connect('db.sqlite3')
+    cursor =conn.cursor()
+    cursor.execute("SELECT  Months_on_book,Months_Inactive_12_mon,Credit_Limit,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1, Attrition_Flag FROM  data_credit_card order by random() LIMIT {} ".format(int(nb)))  
+    data = cursor.fetchall()
+    f_data = data
+    cursor.close()
+    f = open("model.sav", "rb")
+    mdl = pickle.load(f)
+    f.close()
+    data=np.array(data)
+    data=pd.DataFrame(data, columns=['Months_on_book','Months_Inactive_12_mon','Credit_Limit','Avg_Open_To_Buy','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt','Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Attrition_Flag'])
+    data = DataFrame(data)
+    data = data.drop('Attrition_Flag',axis=1)
+    y_pred = mdl.predict(data)
+    y_pred = np.array(y_pred)
+    y_pred = pd.DataFrame(y_pred)   
+    y_pred =DataFrame(y_pred)
+    f_data = DataFrame(f_data) 
+    v = pd.concat([f_data,y_pred], axis=1)
+    headings = ('Months_Inactive_12_mon','Months_Inactive_12_mon','Credit_Limit','Avg_Open_To_Buy','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt','Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Attrition_Flag','Prediction')
+    return render_template('resultat.html',headings= headings ,data=v.values)    
 
 
 if __name__ == '__main__':
